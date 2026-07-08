2dfa56062bef87bca09e0733a4cb7405c6c4f6da
Pooja Goswami <apoojagoswami5@gmail.com>
2026-07-08 18:00:12 +0530

    Added reusable dashboard structure and active sidebar

---
+++ css/js/app.js
@@ -0,0 +1,16 @@
+const menuItems = document.querySelectorAll(".menu-item");
+
+menuItems.forEach(item => {
+
+    item.addEventListener("click", () => {
+
+        menuItems.forEach(menu => {
+            menu.classList.remove("active");
+        });
+
+        item.classList.add("active");
+
+    });
+
+});
+const courseContent = document.getElementById("course-content");
